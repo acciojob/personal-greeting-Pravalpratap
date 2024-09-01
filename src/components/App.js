6b47a8bceeb9342d1b3e08b6bb8708424c9f78d8
Praval <pravalp949@gmail.com>
2024-09-01 14:03:50 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[state,setState]=useState("")
   return (
     <div>
-        {/* Do not remove the main div */}
+         <label htmlFor="" >Enter your name:</label>
+         <input type="text" value={state} onChange={(e)=>{
+               setState(e.target.value);
+         }} />
+
+         <p>{state}</p>
     </div>
   )
 }
 
 export default App
+
